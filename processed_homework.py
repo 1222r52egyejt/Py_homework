f12a5decb53ae55368eda55750422d1aac6acd60
1222r52egyejt <826030519@qq.com>
2022-07-08 16:17:19 +0800

    homework

---
+++ processed_homework.py
@@ -0,0 +1,23 @@
+def choiceNum(maxNum, stepNum):   
+    data = []
+    delData = []
+    num = 0
+    for i in range(1,maxNum + 1):
+        data.append(i)
+
+    while len(data) > 1:
+        num += 1
+        temp = data.pop(0)
+        if num == stepNum:
+            delData.append(temp)
+            num = 0
+        else:
+            data.append(temp)
+    return{
+        'lastData': data[0],
+        'delData': delData
+    }
+
+result = choiceNum(34, 3)
+print('最后剩下的是第',result['lastData'],'人')
+print('淘汰顺序为',result['delData'])
